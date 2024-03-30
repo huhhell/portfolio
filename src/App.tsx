87f455bd4d13d80dc566303b8ed2848a21b9a451
huhhell <vika20020807@mail.ru>
2024-03-30 14:12:41 +0700

    create template, add styled components

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+function App() {
+
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+const Container = styled.div `
+  max-width: 1400px;
+  margin: 0 auto;
+  background: #222;
+  width: 100vw;
+  height: 100vh;
+`
+
+export default App
