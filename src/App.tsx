ddac2366870f3f975c32deecad7cb6334d8ab646
huhhell <vika20020807@mail.ru>
2024-04-02 21:52:58 +0700

    add activeProject state to create dynamic project page

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,24 @@ import styled from "styled-components";
 import Navigation from "./assets/components/Navigation/Navigation.tsx";
 import {BrowserRouter} from "react-router-dom";
 import Routes from "./assets/Routes/Routes.tsx";
+import {useState} from "react";
 
 // <Link to='products' className='header__shop'>Shop</Link>
 
 // todo fix bug when refresh page state is gone but pages didnt change to initial in nav
 function App() {
+    const [activeProjectId, setActiveProjectId] = useState(1);
+    // const [activePage, setActivePage] = useState('');
+
+    function openProject(projectId: number) {
+        setActiveProjectId(projectId)
+    }
 
   return (
     <Container>
         <BrowserRouter>
             <Navigation/>
-            <Routes />
+            <Routes activeProject={activeProjectId} openProject={openProject}/>
         </BrowserRouter>
     </Container>
   )
