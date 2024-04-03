47ee7a2dbb5f21c816bf0049f4ed71ea98984d5f
huhhell <vika20020807@mail.ru>
2024-04-03 12:40:30 +0700

    moved images to another dataFile and add it to projects

---
+++ src/assets/data/images.ts
@@ -0,0 +1,72 @@
+import project01Img01 from '../img/projects/01/01.jpg'
+import project01Img02 from '../img/projects/01/02.jpeg'
+import project01Img03 from '../img/projects/01/03.jpeg'
+import project01Img04 from '../img/projects/01/04.jpeg'
+
+import project02Img01 from '../img/projects/02/01.jpg'
+import project02Img02 from '../img/projects/02/02.jpeg'
+import project02Img03 from '../img/projects/02/03.jpeg'
+import project02Img04 from '../img/projects/02/04.jpeg'
+
+import project03Img01 from '../img/projects/03/01.jpg'
+
+import project04Img01 from '../img/projects/04/01.jpg'
+import project04Img02 from '../img/projects/04/02.jpeg'
+import project04Img03 from '../img/projects/04/03.jpeg'
+import project04Img04 from '../img/projects/04/04.jpeg'
+
+import project05Img01 from '../img/projects/05/01.jpg'
+import project05Img02 from '../img/projects/05/02.jpeg'
+import project05Img03 from '../img/projects/05/03.jpeg'
+import project05Img04 from '../img/projects/05/04.jpeg'
+
+import project06Img01 from '../img/projects/06/01.jpg'
+import project06Img02 from '../img/projects/06/02.jpeg'
+import project06Img03 from '../img/projects/06/03.jpeg'
+import project06Img04 from '../img/projects/06/04.jpeg'
+
+export interface ProjectImage {
+    img: string,
+    alt: string,
+    id: number
+}
+
+export const Project01Images: ProjectImage[] = [
+    {img: project01Img01, alt: 'lorem', id: 0},
+    {img: project01Img02, alt: 'lorem', id: 1},
+    {img: project01Img03, alt: 'lorem', id: 2},
+    {img: project01Img04, alt: 'lorem', id: 3},
+]
+
+export const Project02Images: ProjectImage[] = [
+    {img: project02Img01, alt: 'lorem', id: 0},
+    {img: project02Img02, alt: 'lorem', id: 1},
+    {img: project02Img03, alt: 'lorem', id: 2},
+    {img: project02Img04, alt: 'lorem', id: 3},
+]
+
+export const Project03Images: ProjectImage[] = [
+    {img: project03Img01, alt: 'lorem', id: 0},
+]
+
+export const Project04Images: ProjectImage[] = [
+    {img: project04Img01, alt: 'lorem', id: 0},
+    {img: project04Img02, alt: 'lorem', id: 1},
+    {img: project04Img03, alt: 'lorem', id: 2},
+    {img: project04Img04, alt: 'lorem', id: 3},
+]
+
+export const Project05Images: ProjectImage[] = [
+    {img: project05Img01, alt: 'lorem', id: 0},
+    {img: project05Img02, alt: 'lorem', id: 1},
+    {img: project05Img03, alt: 'lorem', id: 2},
+    {img: project05Img04, alt: 'lorem', id: 3},
+]
+
+export const Project06Images: ProjectImage[] = [
+    {img: project06Img01, alt: 'lorem', id: 0},
+    {img: project06Img02, alt: 'lorem', id: 1},
+    {img: project06Img03, alt: 'lorem', id: 2},
+    {img: project06Img04, alt: 'lorem', id: 3},
+]
+
--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -1,9 +1,3 @@
-import projectImg01 from "../img/projects/01.jpg";
-import projectImg02 from "../img/projects/02.jpg";
-import projectImg03 from "../img/projects/03.jpg";
-import projectImg04 from "../img/projects/04.jpg";
-import projectImg05 from "../img/projects/05.jpg";
-import projectImg06 from "../img/projects/06.jpg";
 import {
     CSS,
     HTML,
@@ -17,20 +11,27 @@ import {
     Tech,
     typeScript
 } from "./tech.ts";
+import {
+    Project01Images,
+    Project02Images, Project03Images,
+    Project04Images,
+    Project05Images,
+    Project06Images,
+    ProjectImage
+} from "./images.ts";
 
-interface ProjectLinks {
+interface ProjectLink {
     name: string,
     link: string,
     id: number,
 }
 
-
 export default interface Project {
     name: string,
     tech: Tech[],
     description: string,
-    img: string,
-    links: ProjectLinks[],
+    img: ProjectImage[],
+    links: ProjectLink[],
     category: string,
     id: number
 }
@@ -40,7 +41,7 @@ export const projects: Project[] = [
         name: 'Studio Retro website',
         tech: [reactT, typeScript],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg01,
+        img: Project01Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'React',
         id: 0
@@ -49,7 +50,7 @@ export const projects: Project[] = [
         name: 'E-Commerce',
         tech: [HTML, SCSS, materialUI],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg02,
+        img: Project02Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'HTML / CSS',
         id: 1
@@ -58,7 +59,7 @@ export const projects: Project[] = [
         name: 'Portfolio',
         tech: [reactT, reactRouter, reactTestingLibrary, jest],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg03,
+        img: Project03Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'Components',
         id: 2
@@ -67,7 +68,7 @@ export const projects: Project[] = [
         name: 'Travel Website',
         tech: [reactT, javaScript, styledComponents],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg04,
+        img: Project04Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'React',
         id: 3
@@ -76,7 +77,7 @@ export const projects: Project[] = [
         name: 'Web Studio',
         tech: [HTML, CSS, typeScript],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg05,
+        img: Project05Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'React',
         id: 4
@@ -85,7 +86,7 @@ export const projects: Project[] = [
         name: 'Movie Website',
         tech: [HTML, CSS, typeScript],
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget commodo quam. Curabitur urna ipsum, vehicula et nibh eu, congue dignissim urna. Integer ornare, libero placerat finibus congue, mauris nulla.',
-        img: projectImg06,
+        img: Project06Images,
         links: [{name: 'GitHub', link: '', id: 0}, {name: 'Website', link: '', id: 2}, {name: 'Notion', link: '', id: 3}],
         category: 'HTML / CSS',
         id: 5
