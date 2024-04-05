90edab3ee39dfeaba81498a6d879cf3ec5e46984
huhhell <vika20020807@mail.ru>
2024-04-05 16:34:53 +0700

    add my own project and images to them

--- src/assets/data/images.ts
+++ src/assets/data/images.ts
@@ -25,6 +25,44 @@ import project06Img02 from '../img/projects/06/02.jpeg'
 import project06Img03 from '../img/projects/06/03.jpeg'
 import project06Img04 from '../img/projects/06/04.jpeg'
 
+import project07Img01 from '../img/projects/bouquets/01.jpeg'
+import project07Img02 from '../img/projects/bouquets/02.jpeg'
+import project07Img03 from '../img/projects/bouquets/03.jpeg'
+import project07Img04 from '../img/projects/bouquets/04.jpeg'
+import project07Img05 from '../img/projects/bouquets/05.jpeg'
+import project07Img06 from '../img/projects/bouquets/06.jpeg'
+
+import project08Img01 from '../img/projects/carsLanding/01.jpeg'
+import project08Img02 from '../img/projects/carsLanding/02.jpeg'
+import project08Img03 from '../img/projects/carsLanding/03.jpeg'
+import project08Img04 from '../img/projects/carsLanding/04.jpeg'
+
+import project09Img01 from '../img/projects/childEducation/01.jpeg'
+import project09Img02 from '../img/projects/childEducation/02.jpeg'
+import project09Img03 from '../img/projects/childEducation/03.jpeg'
+import project09Img04 from '../img/projects/childEducation/04.jpeg'
+import project09Img05 from '../img/projects/childEducation/05.jpeg'
+import project09Img06 from '../img/projects/childEducation/06.jpeg'
+import project09Img07 from '../img/projects/childEducation/07.jpeg'
+import project09Img08 from '../img/projects/childEducation/08.jpeg'
+import project09Img09 from '../img/projects/childEducation/09.jpeg'
+
+import project10Img01 from '../img/projects/creditCardForm/01.jpeg'
+import project10Img02 from '../img/projects/creditCardForm/02.gif'
+
+import project11Img01 from '../img/projects/dashboard/01.jpeg'
+import project11Img02 from '../img/projects/dashboard/02.gif'
+import project11Img03 from '../img/projects/dashboard/03.gif'
+import project11Img04 from '../img/projects/dashboard/04.gif'
+
+import project12Img01 from '../img/projects/hydraLanding/01.jpeg'
+import project12Img02 from '../img/projects/hydraLanding/02.jpeg'
+import project12Img03 from '../img/projects/hydraLanding/03.jpeg'
+import project12Img04 from '../img/projects/hydraLanding/04.jpeg'
+
+import project13Img01 from '../img/projects/todo/01.jpeg'
+import project13Img02 from '../img/projects/todo/02.gif'
+
 export interface ProjectImage {
     img: string,
     alt: string,
@@ -68,5 +106,57 @@ export const Project06Images: ProjectImage[] = [
     {img: project06Img02, alt: 'lorem', id: 1},
     {img: project06Img03, alt: 'lorem', id: 2},
     {img: project06Img04, alt: 'lorem', id: 3},
+]
+
+export const Project07Images: ProjectImage[] = [
+    {img: project07Img01, alt: 'lorem', id: 0},
+    {img: project07Img02, alt: 'lorem', id: 1},
+    {img: project07Img03, alt: 'lorem', id: 2},
+    {img: project07Img04, alt: 'lorem', id: 3},
+    {img: project07Img05, alt: 'lorem', id: 4},
+    {img: project07Img06, alt: 'lorem', id: 5},
+]
+
+export const Project08Images: ProjectImage[] = [
+    {img: project08Img01, alt: 'lorem', id: 0},
+    {img: project08Img02, alt: 'lorem', id: 1},
+    {img: project08Img03, alt: 'lorem', id: 2},
+    {img: project08Img04, alt: 'lorem', id: 3},
+]
+
+export const Project09Images: ProjectImage[] = [
+    {img: project09Img01, alt: 'lorem', id: 0},
+    {img: project09Img02, alt: 'lorem', id: 1},
+    {img: project09Img03, alt: 'lorem', id: 2},
+    {img: project09Img04, alt: 'lorem', id: 3},
+    {img: project09Img05, alt: 'lorem', id: 4},
+    {img: project09Img06, alt: 'lorem', id: 5},
+    {img: project09Img07, alt: 'lorem', id: 6},
+    {img: project09Img08, alt: 'lorem', id: 7},
+    {img: project09Img09, alt: 'lorem', id: 8},
+]
+
+export const Project10Images: ProjectImage[] = [
+    {img: project10Img01, alt: 'lorem', id: 0},
+    {img: project10Img02, alt: 'lorem', id: 1},
+]
+
+export const Project11Images: ProjectImage[] = [
+    {img: project11Img01, alt: 'lorem', id: 0},
+    {img: project11Img02, alt: 'lorem', id: 1},
+    {img: project11Img03, alt: 'lorem', id: 2},
+    {img: project11Img04, alt: 'lorem', id: 3},
+]
+
+export const Project12Images: ProjectImage[] = [
+    {img: project12Img01, alt: 'lorem', id: 0},
+    {img: project12Img02, alt: 'lorem', id: 1},
+    {img: project12Img03, alt: 'lorem', id: 2},
+    {img: project12Img04, alt: 'lorem', id: 3},
+]
+
+export const Project13Images: ProjectImage[] = [
+    {img: project13Img01, alt: 'lorem', id: 0},
+    {img: project13Img02, alt: 'lorem', id: 1},
 ]
 
