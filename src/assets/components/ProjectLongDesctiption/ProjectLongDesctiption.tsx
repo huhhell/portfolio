1d821cc327fd03ef523a1cd028801cc729b59964
huhhell <vika20020807@mail.ru>
2024-04-06 12:51:39 +0700

    fix nesting

--- src/assets/components/ProjectLongDesctiption/ProjectLongDesctiption.tsx
+++ src/assets/components/ProjectLongDesctiption/ProjectLongDesctiption.tsx
@@ -3,7 +3,7 @@ import {ReactNode} from "react";
 type PropsWithChildren<P> = P & { children?: ReactNode };
 
 export default function ProjectLongDescription({children}: PropsWithChildren<any>) {
-    return <p>
+    return <>
         {children}
-    </p>
+    </>
 }
