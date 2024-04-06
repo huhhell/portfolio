ee70c1974717b07d650cb441be56eef8f35762f7
huhhell <vika20020807@mail.ru>
2024-04-06 16:30:09 +0700

    fix names

--- src/assets/data/images.ts
+++ src/assets/data/images.ts
@@ -1,67 +1,35 @@
-import project01Img01 from '../img/projects/01/01.jpg'
-import project01Img02 from '../img/projects/01/02.jpeg'
-import project01Img03 from '../img/projects/01/03.jpeg'
-import project01Img04 from '../img/projects/01/04.jpeg'
+import project01Img01 from '../img/projects/bouquets/01.jpeg'
+import project01Img02 from '../img/projects/bouquets/02.jpeg'
+import project01Img03 from '../img/projects/bouquets/03.jpeg'
+import project01Img04 from '../img/projects/bouquets/04.jpeg'
+import project01Img05 from '../img/projects/bouquets/05.jpeg'
+import project01Img06 from '../img/projects/bouquets/06.jpeg'
 
-import project02Img01 from '../img/projects/02/01.jpg'
-import project02Img02 from '../img/projects/02/02.jpeg'
-import project02Img03 from '../img/projects/02/03.jpeg'
-import project02Img04 from '../img/projects/02/04.jpeg'
+import project02Img01 from '../img/projects/childEducation/01.jpeg'
+import project02Img02 from '../img/projects/childEducation/02.jpeg'
+import project02Img03 from '../img/projects/childEducation/03.jpeg'
+import project02Img04 from '../img/projects/childEducation/04.jpeg'
+import project02Img05 from '../img/projects/childEducation/05.jpeg'
+import project02Img06 from '../img/projects/childEducation/06.jpeg'
+import project02Img07 from '../img/projects/childEducation/07.jpeg'
+import project02Img08 from '../img/projects/childEducation/08.jpeg'
+import project02Img09 from '../img/projects/childEducation/09.jpeg'
 
-import project03Img01 from '../img/projects/03/01.jpg'
+import project03Img01 from '../img/projects/creditCardForm/01.jpeg'
+import project03Img02 from '../img/projects/creditCardForm/02.gif'
 
-import project04Img01 from '../img/projects/04/01.jpg'
-import project04Img02 from '../img/projects/04/02.jpeg'
-import project04Img03 from '../img/projects/04/03.jpeg'
-import project04Img04 from '../img/projects/04/04.jpeg'
+import project04Img01 from '../img/projects/dashboard/01.jpeg'
+import project04Img02 from '../img/projects/dashboard/02.gif'
+import project04Img03 from '../img/projects/dashboard/03.gif'
+import project04Img04 from '../img/projects/dashboard/04.gif'
 
-import project05Img01 from '../img/projects/05/01.jpg'
-import project05Img02 from '../img/projects/05/02.jpeg'
-import project05Img03 from '../img/projects/05/03.jpeg'
-import project05Img04 from '../img/projects/05/04.jpeg'
+import project05Img01 from '../img/projects/hydraLanding/01.jpeg'
+import project05Img02 from '../img/projects/hydraLanding/02.jpeg'
+import project05Img03 from '../img/projects/hydraLanding/03.jpeg'
+import project05Img04 from '../img/projects/hydraLanding/04.jpeg'
 
-import project06Img01 from '../img/projects/06/01.jpg'
-import project06Img02 from '../img/projects/06/02.jpeg'
-import project06Img03 from '../img/projects/06/03.jpeg'
-import project06Img04 from '../img/projects/06/04.jpeg'
-
-import project07Img01 from '../img/projects/bouquets/01.jpeg'
-import project07Img02 from '../img/projects/bouquets/02.jpeg'
-import project07Img03 from '../img/projects/bouquets/03.jpeg'
-import project07Img04 from '../img/projects/bouquets/04.jpeg'
-import project07Img05 from '../img/projects/bouquets/05.jpeg'
-import project07Img06 from '../img/projects/bouquets/06.jpeg'
-
-import project08Img01 from '../img/projects/carsLanding/01.jpeg'
-import project08Img02 from '../img/projects/carsLanding/02.jpeg'
-import project08Img03 from '../img/projects/carsLanding/03.jpeg'
-import project08Img04 from '../img/projects/carsLanding/04.jpeg'
-
-import project09Img01 from '../img/projects/childEducation/01.jpeg'
-import project09Img02 from '../img/projects/childEducation/02.jpeg'
-import project09Img03 from '../img/projects/childEducation/03.jpeg'
-import project09Img04 from '../img/projects/childEducation/04.jpeg'
-import project09Img05 from '../img/projects/childEducation/05.jpeg'
-import project09Img06 from '../img/projects/childEducation/06.jpeg'
-import project09Img07 from '../img/projects/childEducation/07.jpeg'
-import project09Img08 from '../img/projects/childEducation/08.jpeg'
-import project09Img09 from '../img/projects/childEducation/09.jpeg'
-
-import project10Img01 from '../img/projects/creditCardForm/01.jpeg'
-import project10Img02 from '../img/projects/creditCardForm/02.gif'
-
-import project11Img01 from '../img/projects/dashboard/01.jpeg'
-import project11Img02 from '../img/projects/dashboard/02.gif'
-import project11Img03 from '../img/projects/dashboard/03.gif'
-import project11Img04 from '../img/projects/dashboard/04.gif'
-
-import project12Img01 from '../img/projects/hydraLanding/01.jpeg'
-import project12Img02 from '../img/projects/hydraLanding/02.jpeg'
-import project12Img03 from '../img/projects/hydraLanding/03.jpeg'
-import project12Img04 from '../img/projects/hydraLanding/04.jpeg'
-
-import project13Img01 from '../img/projects/todo/01.jpeg'
-import project13Img02 from '../img/projects/todo/02.gif'
+import project06Img01 from '../img/projects/todo/01.jpeg'
+import project06Img02 from '../img/projects/todo/02.gif'
 
 export interface ProjectImage {
     img: string,
@@ -69,11 +37,15 @@ export interface ProjectImage {
     id: number
 }
 
+
+
 export const Project01Images: ProjectImage[] = [
     {img: project01Img01, alt: 'lorem', id: 0},
     {img: project01Img02, alt: 'lorem', id: 1},
     {img: project01Img03, alt: 'lorem', id: 2},
     {img: project01Img04, alt: 'lorem', id: 3},
+    {img: project01Img05, alt: 'lorem', id: 4},
+    {img: project01Img06, alt: 'lorem', id: 5},
 ]
 
 export const Project02Images: ProjectImage[] = [
@@ -81,10 +53,16 @@ export const Project02Images: ProjectImage[] = [
     {img: project02Img02, alt: 'lorem', id: 1},
     {img: project02Img03, alt: 'lorem', id: 2},
     {img: project02Img04, alt: 'lorem', id: 3},
+    {img: project02Img05, alt: 'lorem', id: 4},
+    {img: project02Img06, alt: 'lorem', id: 5},
+    {img: project02Img07, alt: 'lorem', id: 6},
+    {img: project02Img08, alt: 'lorem', id: 7},
+    {img: project02Img09, alt: 'lorem', id: 8},
 ]
 
 export const Project03Images: ProjectImage[] = [
     {img: project03Img01, alt: 'lorem', id: 0},
+    {img: project03Img02, alt: 'lorem', id: 1},
 ]
 
 export const Project04Images: ProjectImage[] = [
@@ -104,59 +82,5 @@ export const Project05Images: ProjectImage[] = [
 export const Project06Images: ProjectImage[] = [
     {img: project06Img01, alt: 'lorem', id: 0},
     {img: project06Img02, alt: 'lorem', id: 1},
-    {img: project06Img03, alt: 'lorem', id: 2},
-    {img: project06Img04, alt: 'lorem', id: 3},
-]
-
-export const Project07Images: ProjectImage[] = [
-    {img: project07Img01, alt: 'lorem', id: 0},
-    {img: project07Img02, alt: 'lorem', id: 1},
-    {img: project07Img03, alt: 'lorem', id: 2},
-    {img: project07Img04, alt: 'lorem', id: 3},
-    {img: project07Img05, alt: 'lorem', id: 4},
-    {img: project07Img06, alt: 'lorem', id: 5},
-]
-
-export const Project08Images: ProjectImage[] = [
-    {img: project08Img01, alt: 'lorem', id: 0},
-    {img: project08Img02, alt: 'lorem', id: 1},
-    {img: project08Img03, alt: 'lorem', id: 2},
-    {img: project08Img04, alt: 'lorem', id: 3},
-]
-
-export const Project09Images: ProjectImage[] = [
-    {img: project09Img01, alt: 'lorem', id: 0},
-    {img: project09Img02, alt: 'lorem', id: 1},
-    {img: project09Img03, alt: 'lorem', id: 2},
-    {img: project09Img04, alt: 'lorem', id: 3},
-    {img: project09Img05, alt: 'lorem', id: 4},
-    {img: project09Img06, alt: 'lorem', id: 5},
-    {img: project09Img07, alt: 'lorem', id: 6},
-    {img: project09Img08, alt: 'lorem', id: 7},
-    {img: project09Img09, alt: 'lorem', id: 8},
-]
-
-export const Project10Images: ProjectImage[] = [
-    {img: project10Img01, alt: 'lorem', id: 0},
-    {img: project10Img02, alt: 'lorem', id: 1},
-]
-
-export const Project11Images: ProjectImage[] = [
-    {img: project11Img01, alt: 'lorem', id: 0},
-    {img: project11Img02, alt: 'lorem', id: 1},
-    {img: project11Img03, alt: 'lorem', id: 2},
-    {img: project11Img04, alt: 'lorem', id: 3},
-]
-
-export const Project12Images: ProjectImage[] = [
-    {img: project12Img01, alt: 'lorem', id: 0},
-    {img: project12Img02, alt: 'lorem', id: 1},
-    {img: project12Img03, alt: 'lorem', id: 2},
-    {img: project12Img04, alt: 'lorem', id: 3},
-]
-
-export const Project13Images: ProjectImage[] = [
-    {img: project13Img01, alt: 'lorem', id: 0},
-    {img: project13Img02, alt: 'lorem', id: 1},
 ]
 
--- src/assets/data/projects.tsx
+++ src/assets/data/projects.tsx
@@ -8,22 +8,15 @@ import {
     reactT,
     reactTestingLibrary,
     SCSS,
-    // styledComponents,
     Tech,
     typeScript
 } from "./tech.ts";
 import {
-    // Project01Images,
-    // Project02Images,
-    // Project03Images,
-    // Project04Images,
-    // Project05Images,
-    // Project06Images,
-    Project07Images,
-    Project09Images,
-    Project10Images,
-    Project11Images,
-    Project12Images, Project13Images,
+    Project01Images,
+    Project02Images,
+    Project03Images,
+    Project04Images,
+    Project05Images, Project06Images,
     ProjectImage
 } from "./images.ts";
 import ProjectLongDescription from "../components/ProjectLongDesctiption/ProjectLongDesctiption.tsx";
@@ -60,7 +53,7 @@ export const projects: Project[] = [
             главной странице в десктопной версии реализованы с sticky-эффектом. Создана карусель с отзывами. Страница
             подписки на цветы содержит динамический FAQ. Весь сайт доступен для ПК, планшетов и мобильных устройств.
         </ProjectLongDescription>,
-        img: Project07Images,
+        img: Project01Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/flowerWeb', id: 0}, {name: 'Website', link: 'https://flower-web-sooty.vercel.app/products', id: 2}, {name: 'Notion', link: 'https://www.notion.so/Bouquets-E-Commerce-dbe27757e8c24f2f8411ef505a01baad?pvs=4', id: 3}, {name: 'Figma', link: 'https://www.figma.com/community/file/1259217583079978202/ecommerce-flower-delivery-website-ui-ux-ui-kit-template', id: 4}],
         category: 'React',
         id: 0
@@ -70,7 +63,7 @@ export const projects: Project[] = [
         tech: [HTML, SCSS],
         descriptionShort: 'Необычный, адаптивный сайт-одностраничник, с использованием методологии BEM. ',
         descriptionLong: 'Один из первых моих проектов. Реализована адаптивная верстка, которая хорошо смотрится на пк, планшетных и мобильных устройствах. Использована методология-BEM. Добавлены блоки необычных форм, размеров и расположения. Старалась сохранить их уникальность и на мобильной версии сайта. ',
-        img: Project09Images,
+        img: Project02Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/educationLanding', id: 0}, {name: 'Website', link: 'https://education-landing-five.vercel.app/', id: 2}, {name: 'Notion', link: 'https://www.notion.so/Curious-Kids-Website-6f6a97cd43334c1f915bea183c0e1908?pvs=4', id: 3}, {name: 'Figma', link: 'https://www.figma.com/file/mKjceh1ZnhPYSrYpovrQ7A/Edutech-Landing-Page-(Community)?type=design&node-id=0%3A1&mode=design&t=QttaiDl5LaNa5rVK-1', id: 4}],
         category: 'HTML / CSS',
         id: 1
@@ -80,7 +73,7 @@ export const projects: Project[] = [
         tech: [HTML, SCSS, javaScript],
         descriptionShort: 'Форма для заполнения банковской карты',
         descriptionLong: 'Форма для заполнения данных банковской карты. Сделана в качестве практики анимаций и использования JavaScript в браузере. Из функционала были реализованы: анимация при вводе номера карты, заглушка цифр посередине, работа формы а так же возможность ее отправки на сервер. Дизайн был создан мною, опираясь на похожие примеры. ',
-        img: Project10Images,
+        img: Project03Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/creditCardForm', id: 0}, {name: 'Notion', link: 'https://www.notion.so/credit-card-form-6e1325a527d14c0f94b0a96e700ad6a7?pvs=4', id: 3}],
         category: 'Components',
         id: 2
@@ -90,7 +83,7 @@ export const projects: Project[] = [
         tech: [reactT, javaScript],
         descriptionShort: 'SPA с компонентами для повышения личной продуктивности',
         descriptionLong: 'Небольшое SPA созданное для того чтобы собрать компоненты, которые писала на протяжении своего обучения, воедино. На данный момент состоит из Помодоро таймера, который можно настроить под себя, менеджера задач и трекера привычек. Их функционал можно посмотреть в галерее. Так же присутсвуют блоки с текущей датой и временем. ',
-        img: Project11Images,
+        img: Project04Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/dashboard', id: 0}, {name: 'Website', link: 'https://dashboard-five-blue-84.vercel.app/', id: 2}, {name: 'Notion', link: 'https://www.notion.so/Dashboard-a8527b923d474754af40b8c0fdcfb927?pvs=4', id: 3}, {name: 'Figma', link: 'https://www.figma.com/community/file/1060952013207459371/vision-ui-dashboard-react-mui-dashboard-free-version', id: 4}],
         category: 'React',
         id: 3
@@ -100,7 +93,7 @@ export const projects: Project[] = [
         tech: [HTML, CSS, javaScript],
         descriptionShort: 'Адаптивный лэндинг для компании которая занимается vr разработкой',
         descriptionLong: 'Адаптивная лэндинг страница. Создана по макету из Figma с помощью HTML, SCSS, JavaScript. Блоки со списками адаптированы под мобильное устройство в качестве галерей, так же добавлено бургер-меню.',
-        img: Project12Images,
+        img: Project05Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/hydraLanding', id: 0}, {name: 'Website', link: 'https://hydra-landing-blond.vercel.app/', id: 2}, {name: 'Notion', link: 'https://www.notion.so/HydraLanding-00afb6e077614f748b80fe356e105e96?pvs=4', id: 3}, {name: 'Figma', link: 'https://www.figma.com/community/file/1196146735025729739/hydra-landing-page', id: 4}],
         category: 'HTML / CSS',
         id: 4
@@ -110,7 +103,7 @@ export const projects: Project[] = [
         tech: [reactT, SCSS, typeScript, jest, reactTestingLibrary],
         descriptionShort: 'Менеджер задач с возможностью сортировки по категориям и всеми базовыми, необходимыми функциями.',
         descriptionLong: 'Небольшое приложение менеджер задач. Реализованы функции добавления, удаления и редактирования задачи. Возможность соритровки задач по категориям, добавление новой категории и назначения ей своего собственного цвета. Приложение полностью покрыто e2e, integration и unit тестами (в процессе). ',
-        img: Project13Images,
+        img: Project06Images,
         links: [{name: 'GitHub', link: 'https://github.com/huhhell/TodoApp', id: 0}, {name: 'Website', link: 'https://todo-app-hazel-xi.vercel.app/', id: 2}, {name: 'Notion', link: 'https://www.notion.so/todoAppReact-279d917e7f3a4587a568b3e7211d1ed9?pvs=4', id: 3}, {name: 'Figma', link: 'https://www.figma.com/community/file/1133173477581507595/todo-app', id: 4}],
         category: 'Components',
         id: 5
