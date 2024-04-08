e77f14b8ed05b08d976787b5363e6249ef8af6de
huhhell <vika20020807@mail.ru>
2024-04-08 13:03:15 +0700

    add portfolio project and fix names of images objects

--- src/assets/data/images.ts
+++ src/assets/data/images.ts
@@ -31,13 +31,19 @@ import project05Img04 from '../img/projects/hydraLanding/04.jpeg'
 import project06Img01 from '../img/projects/todo/01.jpeg'
 import project06Img02 from '../img/projects/todo/02.gif'
 
+import project07Img01 from '../img/projects/portfolio/01.jpeg'
+import project07Img02 from '../img/projects/portfolio/02.jpeg'
+import project07Img03 from '../img/projects/portfolio/03.jpeg'
+import project07Img04 from '../img/projects/portfolio/04.jpeg'
+import project07Img05 from '../img/projects/portfolio/05.jpeg'
+
 export interface ProjectImage {
     img: string,
     alt: string,
     id: number
 }
 
-export const Project01Images: ProjectImage[] = [
+export const project01Images: ProjectImage[] = [
     {img: project01Img01, alt: 'lorem', id: 0},
     {img: project01Img02, alt: 'lorem', id: 1},
     {img: project01Img03, alt: 'lorem', id: 2},
@@ -46,7 +52,7 @@ export const Project01Images: ProjectImage[] = [
     {img: project01Img06, alt: 'lorem', id: 5},
 ]
 
-export const Project02Images: ProjectImage[] = [
+export const project02Images: ProjectImage[] = [
     {img: project02Img01, alt: 'lorem', id: 0},
     {img: project02Img02, alt: 'lorem', id: 1},
     {img: project02Img03, alt: 'lorem', id: 2},
@@ -58,27 +64,37 @@ export const Project02Images: ProjectImage[] = [
     {img: project02Img09, alt: 'lorem', id: 8},
 ]
 
-export const Project03Images: ProjectImage[] = [
+export const project03Images: ProjectImage[] = [
     {img: project03Img01, alt: 'lorem', id: 0},
     {img: project03Img02, alt: 'lorem', id: 1},
 ]
 
-export const Project04Images: ProjectImage[] = [
+export const project04Images: ProjectImage[] = [
     {img: project04Img01, alt: 'lorem', id: 0},
     {img: project04Img02, alt: 'lorem', id: 1},
     {img: project04Img03, alt: 'lorem', id: 2},
     {img: project04Img04, alt: 'lorem', id: 3},
 ]
 
-export const Project05Images: ProjectImage[] = [
+export const project05Images: ProjectImage[] = [
     {img: project05Img01, alt: 'lorem', id: 0},
     {img: project05Img02, alt: 'lorem', id: 1},
     {img: project05Img03, alt: 'lorem', id: 2},
     {img: project05Img04, alt: 'lorem', id: 3},
 ]
 
-export const Project06Images: ProjectImage[] = [
+export const project06Images: ProjectImage[] = [
     {img: project06Img01, alt: 'lorem', id: 0},
     {img: project06Img02, alt: 'lorem', id: 1},
 ]
+
+export const project07Images: ProjectImage[] = [
+    {img: project07Img01, alt: 'lorem', id: 0},
+    {img: project07Img02, alt: 'lorem', id: 1},
+    {img: project07Img03, alt: 'lorem', id: 2},
+    {img: project07Img04, alt: 'lorem', id: 3},
+    {img: project07Img05, alt: 'lorem', id: 4},
+]
+
+
 
